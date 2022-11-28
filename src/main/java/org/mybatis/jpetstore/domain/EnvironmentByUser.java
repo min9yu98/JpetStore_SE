1b375d63af90aad7efc3b118c8543697b426a41e
min9yu98 <kmkkms9698@naver.com>
2022-11-29 01:20:06 +0900

    feat: 유저의 환경 설정 기능 추가

---
+++ src/main/java/org/mybatis/jpetstore/domain/EnvironmentByUser.java
@@ -0,0 +1,7 @@
+package org.mybatis.jpetstore.domain;
+
+public class EnvironmentByUser {
+    private static final long serialVersionUID = 6804536240033522157L;
+    private String columnname;
+    private String columnvalue;
+}
